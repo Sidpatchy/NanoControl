a6bf4a4eb0f32c68fbb7630638e40e9c4c180271
Sidpatchy <administrator@sidpatchy.com>
2023-09-20 22:48:10 -0500

    Out with the old

--- src/hardwareIntegration/test.py
+++
@@ -1,8 +0,0 @@
-from sLOUT import readConfig
-
-# Parse data from the config file
-num_pixels = readConfig('../config.yml', 'numPixels')
-numLeaves = readConfig('../config.yml', 'numLeaves')
-neopixelPin = readConfig('../config.yml', 'neopixelPin')
-
-print(neopixelPin)
